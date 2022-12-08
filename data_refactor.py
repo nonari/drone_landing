32317f3178f286b36e9be7307c7910db977ca9b8
nonari <17324278+nonari@users.noreply.github.com>
2022-12-08 19:29:50 +0100

    dataset refactor

--- data_refactor.py
+++ data_refactor.py
@@ -1,13 +1,46 @@
 from os import path
 
+import numpy as np
 from PIL import Image
 
 from config import Config
 from glob import glob
 
 
+tugraz_classes = [
+    "unlabeled",
+    "paved-area",
+    "dirt",
+    "grass",
+    "gravel",
+    "water",
+    "rocks",
+    "pool",
+    "vegetation",
+    "root",
+    "wall",
+    "window",
+    "door",
+    "fence",
+    "fence-pole",
+    "person",
+    "dog",
+    "car",
+    "bicycle",
+    "tree",
+    "bald-tree",
+    "ar-marker",
+    "obstacle",
+    "conflicting"
+]
+
+
 tugraz_new_labels = {
-    1: [0,0,0]
+    6: [13, 14, 22],
+    5: [7],
+    8: [19, 20],
+    9: [10, 11, 12],
+    17: [18]
 }
 
 
@@ -43,7 +76,18 @@ def downsize_tugraz():
         imo.close()
 
 
+def collapse_labels(label_im, equivalence, color_map):
+    v = [256 * 256, 256, 1]
+    one_ch = np.sum(label_im * np.asarray([v]), axis=2)
+    for k in equivalence.keys():
+        key_color = np.sum(color_map[k] * v)
+        other_colors = np.sum(color_map[equivalence[k]] * [v], axis=1)
+        one_ch[np.isin(one_ch, other_colors)] = key_color
 
+    r = one_ch // 256*256
+    p1 = one_ch - 256*256*r
+    g = p1 // 256
+    b = p1 - 256*g
 
-def collapse_labels():
-    pass
+    return np.dstack([r, g, b])
+
