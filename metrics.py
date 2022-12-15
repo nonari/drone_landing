5517c2da4a3dee70426ef78761e4f5ac978b351e
nonari <17324278+nonari@users.noreply.github.com>
2022-12-16 00:45:49 +0100

    fix retrocompatibility

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 
 def calc_acc(output, target):
-    predictions = output.argmax(dim=1, keepdim=True).squeeze()
+    predictions = output.argmax(dim=1, keepdim=True)
     correct = (predictions == target).sum().item()
     accuracy = correct / target.size().numel()
 
