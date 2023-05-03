b01e243fbc2858a127b5f9e7c205a94f531ab291
nonari <17324278+nonari@users.noreply.github.com>
2023-05-04 01:28:09 +0200

    updated aeroscapes dataset

--- datasets/aeroscapes.py
+++ datasets/aeroscapes.py
@@ -1,12 +1,13 @@
 import numpy as np
-from torch.utils.data import Dataset
+from torch.nn import functional
 import torch
 from os import path
 from glob import glob
 from PIL import Image
 import importlib
+from config import TrainConfig
 
-from datasets.dataset import adapt_image, GenericDataset
+from datasets.dataset import augment, adapt_image, GenericDataset, prepare_image, adapt_label
 
 aeroscapes_color = np.asarray([
     [0, 0, 0],
@@ -34,8 +35,7 @@ aeroscapes_classnames = [
 ]
 
 
-def aero_label_to_tensor(label):
-    # Compatibility issue? READ ONLY label
+def label_to_tensor(label, keys):
     label = label.copy()
     label[label == 4] = 0
     label[label == 5] = 0
@@ -45,52 +45,64 @@ def aero_label_to_tensor(label):
     label[label == 8] = 6
     label[label == 9] = 7
     label[label == 10] = 8
-
-    r, c = label.shape
-    sparse_mask = np.zeros((r, c, 9), np.int32)
-    y, x = np.unravel_index(np.arange(r*c), (r, c))
-    sparse_mask[y, x, label.flatten().astype(np.int32)] = 1
-    sparse_mask = sparse_mask.astype(np.float32)
-    return torch.tensor(sparse_mask).movedim(2, 0)
+    sparse = functional.one_hot(torch.tensor(label.astype(int)), num_classes=9)
+    return sparse.movedim(2, 0)
 
 
-def prepare_image(transformation):
-    def f(im_path):
-        im_orig = Image.open(im_path)
-        im_tensor = transformation(im_orig)
-        im_orig.close()
-        return im_tensor
-    return f
+def load_txt(file):
+    names = []
+    with open(file, 'r') as f:
+        for line in f:
+            names.append(line[:-1])
+    return names
 
 
-def label_transformation(new_size):
-    def f(label):
-        lab_res = label.resize(new_size[::-1], Image.NEAREST)
-        return aero_label_to_tensor(np.asarray(lab_res))
-    return f
+def get_all(paths, ids):
+    all_frames = list(filter(lambda p: p[-14:-4] in ids, paths))
+
+    return all_frames
 
 
-class AeroscapesDataset(GenericDataset):
-    def __init__(self, options):
-        images_root = path.join(options.aeroscapes_root, 'JPEGImages')
-        labels_root = path.join(options.aeroscapes_root, 'SegmentationClass')
+class Aeroscapes(GenericDataset):
+    def __init__(self, config):
+        super().__init__(config)
+        if not path.exists(config.aeroscapes_root):
+            raise Exception('Incorrect path for Aeroscapes dataset')
+        self.color_keys = aeroscapes_color
+        images_root = path.join(config.aeroscapes_root, 'JPEGImages')
+        labels_root = path.join(config.aeroscapes_root, 'SegmentationClass')
+        train_set_path = path.join(config.aeroscapes_root, 'ImageSets', 'trn.txt')
+        val_set_path = path.join(config.aeroscapes_root, 'ImageSets', 'val.txt')
+
+        self._train_ids = load_txt(train_set_path)
+        self._val_ids = load_txt(val_set_path)
 
         image_paths = glob(images_root + '/*.jpg')
         label_paths = glob(labels_root + '/*.png')
-        image_paths = sorted(image_paths, key=lambda x: path.basename(x)[:-4])
-        label_paths = sorted(label_paths, key=lambda x: path.basename(x)[:-4])
+        self._image_paths = sorted(image_paths, key=lambda x: path.basename(x)[:-4])
+        self._label_paths = sorted(label_paths, key=lambda x: path.basename(x)[:-4])
 
-        self._image_paths = np.asarray(image_paths)
-        self._label_paths = np.asarray(label_paths)
+        self.inv_idx = {}
+        self.index()
 
-        net_config = importlib.import_module(f'net_configurations.{options.model_config}').CONFIG
-        t_tugraz = adapt_image(net_config['input_size'])
+        net_config = config.net_config
+        t_rural = adapt_image(net_config['input_size'])
 
-        self._prepare_im = prepare_image(t_tugraz)
-        self._prepare_lab = prepare_image(label_transformation(net_config['input_size']))
+        self._prepare_im = prepare_image(t_rural)
+        self._prepare_lab = prepare_image(adapt_label(net_config['input_size']))
+        self._label_to_tensor = label_to_tensor
+        self._no_classes = len(aeroscapes_classnames)
+
+    def index(self):
+        self.inv_idx.clear()
+        for idx, k in enumerate(self._image_paths):
+            self.inv_idx[k] = idx
+
+    def p_to_i(self, p):
+        return [self.inv_idx[k] for k in p]
 
     def classes(self):
-        return 9
+        return self._no_classes
 
     def classnames(self):
         return aeroscapes_classnames
@@ -99,36 +111,26 @@ class AeroscapesDataset(GenericDataset):
         return aeroscapes_color
 
     def pred_to_color_mask(self, true, pred):
-        pred[pred == 2] = 0
-        pred[pred == 4] = 0
-        pred[pred == 5] = 0
-        pred[pred == 6] = 0
-        pred[pred == 7] = 0
-        pred[pred == 8] = 0
-        pred[pred == 19] = 0
-        pred[pred == 23] = 0
-        pred[pred == 15] = 1
-        pred[pred == 18] = 2
-        pred[pred == 17] = 3
-        pred[pred == 16] = 6
-        pred[pred == 22] = 5
-        pred[pred == 13] = 5
-        pred[pred == 14] = 5
-        pred[pred == 9] = 6
-        pred[pred == 10] = 6
-        pred[pred == 11] = 6
-        pred[pred == 12] = 6
-        pred[pred == 3] = 7
-        pred[pred == 9] = 7
-        pred[pred == 20] = 7
-        pred[pred == 21] = 7
-        pred[pred == 1] = 8
         pred_mask = aeroscapes_color[pred]
         true_mask = aeroscapes_color[true]
         return true_mask, pred_mask
+
+    def get_folds(self):
+        train_imgs = get_all(self._image_paths, self._train_ids)
+        val_imgs = get_all(self._image_paths, self._val_ids)
+        if isinstance(self.config, TrainConfig):
+            return [(self.p_to_i(train_imgs), self.p_to_i(val_imgs))]
+        else:
+            return [self.p_to_i(val_imgs)]
 
     def __len__(self):
         return self._image_paths.__len__()
 
     def __getitem__(self, item):
-        return self._prepare_im(self._image_paths[item]),  self._prepare_lab(self._label_paths[item])
+        tensor_im = self._prepare_im(self._image_paths[item])
+        pil_lab = self._prepare_lab(self._label_paths[item])
+        if isinstance(self.config, TrainConfig) and self.config._training and self.config.augment:
+            tensor_im, pil_lab = augment(tensor_im, pil_lab)
+        tensor_lab = self._label_to_tensor(np.asarray(pil_lab), self.color_keys)
+        return tensor_im, tensor_lab
+
